f80083faf58440867d49eede7ad910dd41b86bbd
amalthorakkat <amalthorakkat@gmail.com>
2026-01-22 12:45:06 +0530

    Added header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import UserLayout from "./components/Layout/UserLayout";
+import Home from "./components/Home/Home";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      app
-    </div>
-  )
-}
+      <Routes>
 
-export default App
+        <Route path="/" element={<UserLayout />}>
+       
+        </Route>
+
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
