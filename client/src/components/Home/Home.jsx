eeacbe4901c92400013c4e65cbcd3706e0fb8364
amalthorakkat <amalthorakkat@gmail.com>
2026-01-24 22:55:16 +0530

    Added Our Mission Section

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./homeComponents/Hero";
 import About from "./homeComponents/About";
+import OurMisssion from "./homeComponents/OurMisssion";
 
 const Home = () => {
   return (
     <div>
       <Hero />
-      <About/>
+      <About />
+      <OurMisssion />
     </div>
   );
 };
