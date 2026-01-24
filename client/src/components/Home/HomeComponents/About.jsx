3b2585a08709abbe6434db3dbfbe83ac5e883c08
amalthorakkat <amalthorakkat@gmail.com>
2026-01-24 15:33:44 +0530

    Added About Highlights section

---
+++ client/src/components/Home/HomeComponents/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AboutHighlights from './ui/AboutHighlights'
+
+const About = () => {
+  return (
+    <div>
+        <AboutHighlights/>
+    </div>
+  )
+}
+
+export default About
