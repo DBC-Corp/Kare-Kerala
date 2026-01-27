f45f0201b3f2a78e01e532d90b3a57fcf6f2b146
amalthorakkat <amalthorakkat@gmail.com>
2026-01-27 16:00:24 +0530

    Added section 3 - Why Kerala

--- client/src/components/Home/HomeComponents/WhyKerala.jsx
+++ client/src/components/Home/HomeComponents/WhyKerala.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import WhyKeralaSection1 from './ui/WhyKeralaSection1'
 import WhyKerelaSection2 from './ui/WhyKeralaSection2'
+import WhyKeralaSection3 from './ui/WhyKeralaSection3'
 const WhyKerala = () => {
   return (
 
@@ -8,8 +9,9 @@ const WhyKerala = () => {
     <div>
         <WhyKeralaSection1 />
         <WhyKerelaSection2 />
+        <WhyKeralaSection3 />
           
-    </div>
+    </div>  
   )
 }
 
